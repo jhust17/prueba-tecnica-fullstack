719d3a1c666f823bb2d3b1f9a1163420bc709650
Jhust17 <jhustinallan17@gmail.com>
2026-03-01 16:12:24 -0500

    refactor: remove unnecessary React import in Dashboard component

--- client/src/pages/Dashboard.tsx
+++ client/src/pages/Dashboard.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import type{ Task, TaskStatus } from '../types';
 import { fetchApi } from '../services/api';
